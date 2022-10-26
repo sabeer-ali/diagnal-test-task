2a7d77d9a8056c53cc2142c900588ffbf592a84a
Sabeer ali <sabeer@sectorqube.com>
2022-10-26 13:45:27 +0530

    test task

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 
-import Navbar from "./components/Navbar";
-import Lists from "./components/Lists";
-
 import page1 from "./services/CONTENTLISTINGPAGE-PAGE1.json";
 import page2 from "./services/CONTENTLISTINGPAGE-PAGE2.json";
 import page3 from "./services/CONTENTLISTINGPAGE-PAGE3.json";
-
 import { addListsAction } from "./redux/actions/addallLIst";
-const person = React.lazy(() =>
-  import("./services/CONTENTLISTINGPAGE-PAGE1.json")
-);
+
+const Navbar = React.lazy(() => import("./components/Navbar"));
+const Lists = React.lazy(() => import("./components/Lists"));
 
 function App(props) {
   useEffect(() => {
